50f978d2bac203722222412c3aaea9129581dd53
Byron Henze <byronh@gmail.com>
2016-01-11 19:06:36 -0800

    Switch to gpiozero, auto-restart daemons

--- circuits.py
+++ circuits.py
@@ -2,35 +2,32 @@ import datetime
 import requests
 import time
 
-import RPi.GPIO as io
+import gpiozero as io
 
-bounce_time = 100
 host = 'http://localhost:8080'
-pir_pin = 18
-
-io.setmode(io.BCM)
-io.setup(pir_pin, io.IN, pull_up_down=io.PUD_DOWN)
+pir = io.MotionSensor(pin=18)
 
 
-def on_motion(channel):
-    time.sleep(1)
-    if not io.input(pir_pin):
+def loop():
+    pir.wait_for_active()
+    time.sleep(0.5)
+    if pir.value != 1:
         return
-    io.remove_event_detect(pir_pin)
 
     now = datetime.datetime.now()
     if 16 <= now.hour <= 23 or 0 <= now.hour <= 5:
         requests.get('{}/api/lights/on'.format(host))
+        print('Lights activated')
         if now.weekday() <= 4 and 16 <= now.hour <= 21:
             requests.get('{}/api/desktop/on'.format(host))
+            print('Magic packet sent to desktop')
     elif 7 <= now.hour <= 9:
         requests.get('{}/api/lights/off'.format(host))
+        print('Lights deactivated')
 
     time.sleep(30)
-    io.add_event_detect(pir_pin, io.RISING, callback=on_motion, bouncetime=bounce_time)
 
 
-io.add_event_detect(pir_pin, io.RISING, callback=on_motion, bouncetime=bounce_time)
-
-while True:
-    time.sleep(10)
+if __name__ == '__main__':
+    while True:
+        loop()
